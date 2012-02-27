1a931d6b9a287bda5f018335a097824fed14143a
Alexey Rozhnovskiy <lexx@ixc.ua>
2012-02-27 14:44:04 +0200

    env for win added

---
+++ _vc_2008.1.bat
@@ -0,0 +1,19 @@
+set QTDIR=D:\installenv\qt-everywhere-opensource-src-4.7.4
+set PYTHONPATH=C:\Python32\libs
+set BOOSTDIR=D:\installenv\boost_1_47_0
+
+set RESIPDIR=D:\installenv\resiprocate\main\
+
+set ZLIBDIR=D:\installenv\zlib
+	
+set QSERIALDEVICE=D:\installenv\QextSerialPort
+
+
+set QTMOCADVANCED=1
+set QTMOCINCLUDES=stdafx.h
+
+call "%VS90COMNTOOLS%vsvars32.bat"
+
+devenv
+
+
